da20ab9ec8dae057adb9331c96af206b618126d6
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-01-22 21:09:07 +0530

    Navbar CSS improved !

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,36 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/navbar.css'
-import Divider from '@mui/material/Divider';
-import { dark } from "@mui/material/styles/createPalette";
 
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <nav className="navbar-contents">
-            <Link to="/" className="navbar-links">
-              Home
-            </Link>
-            <Link to="/tutorial" className="navbar-links">
-              Tutorial
-            </Link>
-            <Link to="/mygallery" className="navbar-links">
-              My Gallery
-            </Link>
-            <Link to="/buycredits" className="navbar-links">
-              Buy Credits
-            </Link>
-            <Link to="/aboutus" className="navbar-links">
-              About Us
-            </Link>
-            <Link to="/aboutus">
-            <button className="navbar-button">
-              Logout
-            </button>
-            </Link>
-      </nav>
-    </div>
+    <nav className="navbar">
+          <Link to="/" className="navbar-links">
+            Home
+          </Link>
+          <Link to="/tutorial" className="navbar-links">
+            Tutorial
+          </Link>
+          <Link to="/mygallery" className="navbar-links">
+            My Gallery
+          </Link>
+          <Link to="/buycredits" className="navbar-links">
+            Buy Credits
+          </Link>
+          <Link to="/aboutus" className="navbar-links">
+            About Us
+          </Link>
+          <Link to="/aboutus" className="navbar-button">
+            Logout
+          </Link>
+    </nav>
   );
 };
 
