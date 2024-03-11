577b1339272b0979ef15a1d525c680cadcc929c4
Isha Khandalekar <isha.khandalekar@somaiya.edu>
2024-03-11 21:14:59 +0530

    navbar

--- frontend/app/_components/ui/Input.tsx
+++ frontend/app/_components/ui/Input.tsx
@@ -40,7 +40,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            `flex h-10 w-full border-none bg-gray-50 text-black shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+            `flex h-10 w-full border-none bg-neutral-200 text-black shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
           file:text-sm file:font-medium placeholder-text-neutral-600 
           focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-neutral-600
            disabled:cursor-not-allowed disabled:opacity-50
