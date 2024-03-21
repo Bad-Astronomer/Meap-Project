48d322398b51396a014a3a8547c5e73d4634fbd6
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-03-22 04:46:18 +0530

    Hero fixed

--- frontend/app/home/Home_before_auth.tsx
+++ frontend/app/home/Home_before_auth.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Home_before_auth = () => {
 
-    const titles = ["Remember", "Dream", "See", "Desire"];
+    const titles = ["See", "Dream", "Desire", "Remember"];
     const [titleIndex, setTitleIndex] = useState(0);
 
     useEffect(() => {
@@ -18,15 +18,16 @@ const Home_before_auth = () => {
     return (
         // Outer Container
         //border border-y-transparent
-        <div className='w-4/5 mx-auto p-20 pt-4'>
+        <div className='w-4/5 mx-auto p-20 pt-16'>
             <div className="flex justify-center items-center flex-col">
-                <p className='text-white/[0.75] text-6xl font-bold text-center transform translate-y-20'>
+                <p className='pt-8 h-20 text-white/[0.75] text-6xl font-bold text-center transform translate-y-10 z-10'>
                     <span className='meap-header'>Color</span> it like <br />
-                    you <span className='meap-header'>{titles[titleIndex]}</span> it. </p>
+                    you <span className='meap-header'>{titles[titleIndex]}</span> it.
+                </p>
 
                 <Image
                 src={`/home/image_${titleIndex}.png`}
-                className="h-full w-4/5 object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                className="h-full w-4/6 object-cover object-left-top rounded-lg gap-10 !m-0 !p-0 transform"
                 height="1200"
                 width="1500"
                 alt="thumbnail"
