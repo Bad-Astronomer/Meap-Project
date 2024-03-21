77d95218d7e00397071336bb0c9e07a9dd5b993f
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-03-21 12:54:18 +0530

    Gallery fix

--- frontend/app/_components/ui/ParallaxGrid.tsx
+++ frontend/app/_components/ui/ParallaxGrid.tsx
@@ -42,34 +42,18 @@ export const ParallaxGrid = ({
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start  max-w-6xl mx-auto gap-10 pb-20 px-10"
         ref={gridRef}
       >
-        {/* <div className="grid gap-10">
-          {firstPart.map((el, idx) => (
-            <motion.div
-              style={{ y: translateFirst }} // Apply the translateY motion value here
-              key={"grid-1" + idx}
-            >
-              <Image
-                src={el}
-                className="h-full w-full object-cover object-left-top rounded-lg z-0 gap-10 !m-0 !p-0x"
-                height="400"
-                width="400"
-                alt="thumbnail"
-              />
-            </motion.div>
-          ))}
-        </div> */}
         <div className="grid gap-10 relative isolate">
           {firstPart.map((el, idx) => (
             <div className="group" key={"grid-1" + idx}>
               <motion.div style={{ y: translateFirst }}>
-                <p className="text-white pointer-events-none absolute text-justify text-xl transition duration-300 delay-100 p-4 bottom-0 z-10 opacity-0 group-hover:opacity-100">
+                <p className="text-white pointer-events-none absolute text-justify text-md transition duration-300 delay-100 p-4 bottom-0 z-10 opacity-0 group-hover:opacity-100 bg-black/[0.5] border border-transparent hover:border-white/[0.5] backdrop-filter backdrop-blur-[3px] m-1px rounded-md">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Obcaecati sed culpa minus ratione id accusantium. Voluptatem
                   cumque aut eaque, repellendus
                 </p>
                 <Image
                   src={el}
-                  className="h-full w-full object-cover object-left-top rounded-lg z-0 gap-10 !m-0 !p-0 hover:saturate-0 transition duration-200"
+                  className="h-full w-full object-cover object-left-top rounded-lg z-0 gap-10 !m-0 !p-0 hover:saturate-0 border border-transparent hover:border-white hover:border-opacity-50 transition duration-200"
                   height="400"
                   width="400"
                   alt="thumbnail"
@@ -78,30 +62,44 @@ export const ParallaxGrid = ({
             </div>
           ))}
         </div>
-        <div className="grid gap-10">
+        <div className="grid gap-10 relative isolate">
           {secondPart.map((el, idx) => (
-            <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
-              <Image
-                src={el}
-                className="h-full w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0 hover:saturate-0 transition duration-200"
-                height="400"
-                width="400"
-                alt="thumbnail"
-              />
-            </motion.div>
+            <div className="group" key={"grid-2" + idx}>
+              <motion.div style={{ y: translateSecond }}>
+                <p className="text-white pointer-events-none absolute text-justify text-md transition duration-300 delay-100 p-4 bottom-0 z-10 opacity-0 group-hover:opacity-100 bg-black/[0.5] border border-transparent hover:border-white/[0.5] backdrop-filter backdrop-blur-[3px] m-1px rounded-md">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati sed culpa minus ratione id accusantium. Voluptatem
+                  cumque aut eaque, repellendus
+                </p>
+                <Image
+                  src={el}
+                  className="h-full w-full object-cover object-left-top rounded-lg z-0 gap-10 !m-0 !p-0 hover:saturate-0 border border-transparent hover:border-white hover:border-opacity-50 transition duration-200"
+                  height="400"
+                  width="400"
+                  alt="thumbnail"
+                />
+              </motion.div>
+            </div>
           ))}
         </div>
-        <div className="grid gap-10">
+        <div className="grid gap-10 relative isolate">
           {thirdPart.map((el, idx) => (
-            <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
-              <Image
-                src={el}
-                className="h-full w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0 hover:saturate-0 transition duration-200"
-                height="400"
-                width="400"
-                alt="thumbnail"
-              />
-            </motion.div>
+            <div className="group" key={"grid-3" + idx}>
+              <motion.div style={{ y: translateThird }}>
+                <p className="text-white pointer-events-none absolute text-justify text-md transition duration-300 delay-100 p-4 bottom-0 z-10 opacity-0 group-hover:opacity-100 bg-black/[0.5] border border-transparent hover:border-white/[0.5] backdrop-filter backdrop-blur-[3px] m-1px rounded-md">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati sed culpa minus ratione id accusantium. Voluptatem
+                  cumque aut eaque, repellendus
+                </p>
+                <Image
+                  src={el}
+                  className="h-full w-full object-cover object-left-top rounded-lg z-0 gap-10 !m-0 !p-0 hover:saturate-0 border border-transparent hover:border-white hover:border-opacity-50 transition duration-200"
+                  height="400"
+                  width="400"
+                  alt="thumbnail"
+                />
+              </motion.div>
+            </div>
           ))}
         </div>
       </div>
