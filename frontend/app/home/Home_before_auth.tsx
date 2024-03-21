9254a8833ea5838b713821e876e63a79c0673817
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-03-22 04:34:52 +0530

    [Major] Hero Home

--- frontend/app/home/Home_before_auth.tsx
+++ frontend/app/home/Home_before_auth.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 const Home_before_auth = () => {
+
+    const titles = ["Remember", "Dream", "See", "Desire"];
+    const [titleIndex, setTitleIndex] = useState(0);
+
+    useEffect(() => {
+       const intervalId = setInterval(() => {
+         setTitleIndex((prevTitleIndex) => (prevTitleIndex + 1) % titles.length);
+       }, 2000);
+   
+       return () => clearInterval(intervalId);
+    }, [titleIndex]); 
+
+
     return (
         // Outer Container
-        <div className='w-4/5 mx-auto my-10 p-10 border border-white'>
-            
+        //border border-y-transparent
+        <div className='w-4/5 mx-auto p-20 pt-4'>
+            <div className="flex justify-center items-center flex-col">
+                <p className='text-white/[0.75] text-6xl font-bold text-center transform translate-y-20'>
+                    <span className='meap-header'>Color</span> it like <br />
+                    you <span className='meap-header'>{titles[titleIndex]}</span> it. </p>
+
+                <Image
+                src={`/home/image_${titleIndex}.png`}
+                className="h-full w-4/5 object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                height="1200"
+                width="1500"
+                alt="thumbnail"
+                />
+            </div>
         </div>
     )
 }
