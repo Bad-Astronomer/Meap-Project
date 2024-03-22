f736bd5094a9b440e739265f369f42977afecf99
Bad-Astronomer <kedarmakulkarni@gmail.com>
2024-03-22 11:43:57 +0530

    Bento grid skeleton added

--- frontend/app/home/BentoGridHome.tsx
+++ frontend/app/home/BentoGridHome.tsx
@@ -12,7 +12,7 @@ import {
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-export function BentoGridThirdDemo() {
+export function BentoGridHome() {
     return (
         <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
         {items.map((item, i) => (
@@ -29,7 +29,7 @@ export function BentoGridThirdDemo() {
     );
 }
 const Skeleton = () => (
-    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
+    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-dot-white/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent border-white/[0.2] bg-black"></div>
 );
 
 const SkeletonOne = () => {
@@ -62,28 +62,28 @@ const SkeletonOne = () => {
         <motion.div
         initial="initial"
         whileHover="animate"
-        className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+        className="flex flex-1 w-full h-full min-h-[6rem] bg-dot-white/[0.2] flex-col space-y-2"
         >
         <motion.div
             variants={variants}
-            className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-white dark:bg-black"
+            className="flex flex-row rounded-full border border-white/[0.2] p-2  items-center space-x-2 bg-black"
         >
             <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
-            <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
+            <div className="w-full h-4 rounded-full bg-neutral-900" />
         </motion.div>
         <motion.div
             variants={variantsSecond}
-            className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
+            className="flex flex-row rounded-full border border-white/[0.2] p-2 items-center space-x-2 w-3/4 ml-auto bg-black"
         >
-            <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
+            <div className="w-full h-4 rounded-full bg-neutral-900" />
             <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
         </motion.div>
         <motion.div
             variants={variants}
-            className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 bg-white dark:bg-black"
+            className="flex flex-row rounded-full border border-white/[0.2] p-2 items-center space-x-2 bg-black"
         >
             <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
-            <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
+            <div className="w-full h-4 rounded-full bg-neutral-900" />
         </motion.div>
         </motion.div>
     );
@@ -112,7 +112,7 @@ const SkeletonTwo = () => {
         initial="initial"
         animate="animate"
         whileHover="hover"
-        className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+        className="flex flex-1 w-full h-full min-h-[6rem] bg-dot-white/[0.2] flex-col space-y-2"
         >
         {arr.map((_, i) => (
             <motion.div
@@ -121,7 +121,7 @@ const SkeletonTwo = () => {
             style={{
                 maxWidth: Math.random() * (100 - 40) + 40 + "%",
             }}
-            className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4"
+            className="flex flex-row rounded-full border border-white/[0.2] p-2  items-center space-x-2 bg-black w-full h-4"
             ></motion.div>
         ))}
         </motion.div>
@@ -146,7 +146,7 @@ const SkeletonThree = () => {
             repeat: Infinity,
             repeatType: "reverse",
         }}
-        className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] rounded-lg bg-dot-black/[0.2] flex-col space-y-2"
+        className="flex flex-1 w-full h-full min-h-[6rem] bg-dot-white/[0.2] rounded-lg flex-col space-y-2"
         style={{
             background:
             "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
@@ -183,14 +183,14 @@ const SkeletonFour = () => {
         initial="initial"
         animate="animate"
         whileHover="hover"
-        className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2"
+        className="flex flex-1 w-full h-full min-h-[6rem] bg-dot-white/[0.2] flex-row space-x-2"
         >
         <motion.div
             variants={first}
-            className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+            className="h-full w-1/3 rounded-2xl p-4 bg-black border-white/[0.1] border flex flex-col items-center justify-center"
         >
             <Image
-            src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+            src="/assets/4.jpg"
             alt="avatar"
             height="100"
             width="100"
@@ -199,13 +199,13 @@ const SkeletonFour = () => {
             <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
             Just code in Vanilla Javascript
             </p>
-            <p className="border border-red-500 bg-red-100 dark:bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
+            <p className="border border-red-500 bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
             Delusional
             </p>
         </motion.div>
-        <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
+        <motion.div className="h-full relative z-20 w-1/3 rounded-2xl p-4 bg-black border-white/[0.1] border flex flex-col items-center justify-center">
             <Image
-            src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+            src="/assets/kedar.jpeg"
             alt="avatar"
             height="100"
             width="100"
@@ -214,16 +214,16 @@ const SkeletonFour = () => {
             <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
             Tailwind CSS is cool, you know
             </p>
-            <p className="border border-green-500 bg-green-100 dark:bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
+            <p className="border border-green-500 bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
             Sensible
             </p>
         </motion.div>
         <motion.div
             variants={second}
-            className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+            className="h-full w-1/3 rounded-2xl p-4 bg-black border-white/[0.1] flex flex-col items-center justify-center"
         >
             <Image
-            src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+            src="/assets/himanshu.jpg"
             alt="avatar"
             height="100"
             width="100"
@@ -232,7 +232,7 @@ const SkeletonFour = () => {
             <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
             I love angular, RSC, and Redux.
             </p>
-            <p className="border border-orange-500 bg-orange-100 dark:bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
+            <p className="border border-orange-500 bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
             Helpless
             </p>
         </motion.div>
@@ -269,14 +269,14 @@ const SkeletonFive = () => {
         <motion.div
         initial="initial"
         whileHover="animate"
-        className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+        className="flex flex-1 w-full h-full min-h-[6rem] bg-dot-white/[0.2] flex-col space-y-2"
         >
         <motion.div
             variants={variants}
-            className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-white dark:bg-black"
+            className="flex flex-row rounded-2xl border border-white/[0.2] p-2  items-start space-x-2 bg-black"
         >
             <Image
-            src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+            src="/assets/kedar.jpeg"
             alt="avatar"
             height="100"
             width="100"
@@ -289,7 +289,7 @@ const SkeletonFive = () => {
         </motion.div>
         <motion.div
             variants={variantsSecond}
-            className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
+            className="flex flex-row rounded-full border border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-black"
         >
             <p className="text-xs text-neutral-500">Use PHP.</p>
             <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
--- frontend/app/home/Home_before_auth.tsx
+++ frontend/app/home/Home_before_auth.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { BentoGridHome } from './BentoGridHome';
 
 const Home_before_auth = () => {
 
@@ -18,20 +19,23 @@ const Home_before_auth = () => {
     return (
         // Outer Container
         //border border-y-transparent
-        <div className='w-4/5 mx-auto p-20 pt-16'>
-            <div className="flex justify-center items-center flex-col">
-                <p className='pt-8 h-20 text-white/[0.75] text-6xl font-bold text-center transform translate-y-10 z-10'>
+        <div className='w-4/5 mx-auto p-20 pt-0'>
+            <div className="flex justify-center items-center flex-col pt-16">
+                <p className='pt-16 h-20 text-white/[0.75] text-6xl font-bold text-center transform translate-y-12 z-10'>
                     <span className='meap-header'>Color</span> it like <br />
                     you <span className='meap-header'>{titles[titleIndex]}</span> it.
                 </p>
 
                 <Image
                 src={`/home/image_${titleIndex}.png`}
-                className="h-full w-4/6 object-cover object-left-top rounded-lg gap-10 !m-0 !p-0 transform"
+                className="h-full max-w-4xl object-cover object-left-top rounded-lg gap-10 !m-0 !p-0 transform"
                 height="1200"
                 width="1500"
                 alt="thumbnail"
                 />
+            </div>
+            <div className="mt-20">
+                <BentoGridHome/>
             </div>
         </div>
     )
